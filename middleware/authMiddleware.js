13a85c4e204b21a66be24f52471cd410d88a9757
Ali <aliahmad.06527@gmail.com>
2025-03-12 16:00:59 +0100

    Reso pubblica la rotta /posts/all

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,6 +4,10 @@ const authMiddleware = (req, res, next) => {
   const token = req.header("Authorization")?.replace("Bearer ", "");
   if (!token) return res.status(401).json({ message: "Accesso negato!" });
 
+  if (req.path === "/api/posts/all") {
+    return next();
+  }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
